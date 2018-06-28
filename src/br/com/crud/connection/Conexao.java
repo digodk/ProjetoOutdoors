3a1d8ae4cca5f9256c0679d9765c07d84f3ae79f
digodk <diogodklein@gmail.com>
2018-06-27 22:06:21 -0300

    Criada interface para conexões
    Objetivo da interface é evitar dependência de uma tecnologia em
    específico

---
+++ src/br/com/crud/connection/Conexao.java
@@ -0,0 +1,9 @@
+package br.com.crud.connection;
+
+import java.sql.Connection;
+
+
+// Interface de conexão para evitar que o sistema seja dependente de uma tecnologia em específico
+public interface Conexao extends Connection {
+
+}
