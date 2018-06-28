75b3af5f67e76be3b6fa3c2db5546ffeb3da327a
digodk <diogodklein@gmail.com>
2018-06-27 21:44:51 -0300

    Classe de conexão concluída
    Testador para conexões de BD implementada

--- src/br/com/crud/connection/ConnectionFactory.java
+++ src/br/com/crud/connection/ConnectionFactory.java
@@ -7,12 +7,12 @@ import java.sql.SQLException;
 public class ConnectionFactory {
 
     //Constantes para acessar o banco de dados
-    private final String urlBD = "jdbc:mysql://localhost:3306/projetooutdoors";
-    private final String user = "digodk";
-    private final String password = "35512301";
+    private static final String urlBD = "jdbc:mysql://localhost:3306/projetooutdoors?useTimezone=true&serverTimezone=UTC";
+    private static final String user = "digodk";
+    private static final String password = "35512301";
     
     //Método para retornar a conexão com o banco de dados
-    public Connection obterConexao() {
+    public static Connection obterConexao() {
         
         //Variável para retornar a conex�o
         Connection conexao = null;
@@ -20,10 +20,9 @@ public class ConnectionFactory {
         //Realizar a conexão
         try {
             conexao = DriverManager.getConnection(urlBD, user, password);
-            System.out.println("Conexão com BD realizada com sucesso");
+            
         }catch(SQLException e) {
-            System.out.println("Erro ao estabelecer a conexão");
-            e.printStackTrace();
+           
             throw new RuntimeException(e);
         }
         return conexao;
--- src/br/com/crud/main/Tester.java
+++ src/br/com/crud/main/Tester.java
@@ -1,10 +1,25 @@
 package br.com.crud.main;
 
+import java.sql.Connection;
+
+import br.com.crud.connection.ConnectionFactory;
+
 public class Tester {
 
   public static void main(String[] args) {
     // TODO Auto-generated method stub
+    testarConexoesBD();
 
+  }
+  
+  public static void testarConexoesBD() {
+    try {
+      Connection conexaoBDOutdoors = ConnectionFactory.obterConexao();
+      System.out.println("Conexão com BD realizada com sucesso");
+    } catch(Exception e) {
+      System.out.println("Erro ao estabelecer a conexão");
+      e.printStackTrace();
+    }
   }
 
 }
